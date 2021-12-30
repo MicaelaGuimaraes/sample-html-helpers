950584a452669a47829850ce2465319d7999d81b
Micaela Guimaraes <micaelamarinho321@live.com>
2021-12-30 01:37:03 -0300

    final version with news helpers html

---
+++ SampleHTMLHelpers/Helpers/FileHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleHTMLHelpers.Helpers
+{
+    public static class FileHelper
+    {
+        public static MvcHtmlString File(this HtmlHelper helper, string id)
+        {
+            return File(helper, id, null);
+        }
+
+        public static MvcHtmlString File(this HtmlHelper helper, string id, object htmlAttributes)
+        {
+            return BuildFileHelper(helper, id, htmlAttributes);
+        }
+
+        private static MvcHtmlString BuildFileHelper(this HtmlHelper helper, string id, object attributes)
+        {
+            var tag = new TagBuilder("input");
+            tag.MergeAttribute("type", "file");
+            tag.MergeAttribute("id", id);
+            tag.MergeAttributes(new RouteValueDictionary(attributes));
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
---
+++ SampleHTMLHelpers/Helpers/TagImgHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SampleHTMLHelpers.Helpers
+{
+    public static class TagImgHelper
+    {
+        public static MvcHtmlString TagImg(this HtmlHelper helper, string src, string alt)
+        {
+            return TagImg(helper, src, alt, null);
+        }
+
+        public static MvcHtmlString TagImg(this HtmlHelper helper, string src, string alt, object htmlAttributes)
+        {
+            if (src == null || string.IsNullOrEmpty(src))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            return BuildTagImgHelper(helper, src, alt, htmlAttributes);
+        }
+
+        private static MvcHtmlString BuildTagImgHelper(this HtmlHelper helper, string src, string alt, object htmlAttributes)
+        {
+            var tag = new TagBuilder("img");
+            tag.MergeAttribute("src", src);
+            tag.MergeAttribute("alt", alt);
+            tag.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+
+            return MvcHtmlString.Create(tag.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
